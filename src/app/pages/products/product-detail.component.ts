2e35417ab80c53e34b6119f4ae904d0a5681ca4c
RhiannonAnna <annaandrzejewskagodecka@o2.pl>
2019-11-26 19:18:38 +0100

    Add filtering and Validation

--- src/app/pages/products/product-detail.component.ts
+++ src/app/pages/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
+import { Alert } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,15 +9,13 @@ import { Product } from './product';
 })
 export class ProductDetailComponent implements OnInit {
 
-
   product1: Product = new Product();
 
-  product2: Product = {id: 2, name: 'Ciabata', type: 'Bułka', price: 0.75, weight: 60, description: 'Kwadratowa bułeczka z mąki pszennej i drożdży', quantity: 56};
   constructor() { }
 
   ngOnInit() {
     this.product1.id = 1;
-    this.product1.name = "grahamka";
+    this.product1.name = "";
     this.product1.type = "bułka";
     this.product1.price = 0.90;
     this.product1.weight = 90;
@@ -24,7 +23,62 @@ export class ProductDetailComponent implements OnInit {
     this.product1.quantity = 45;
   }
 
-  alertButton(){
-    alert(JSON.stringify(this.product1))
+  alertButton(): void {
+    // alert(JSON.stringify(this.product1))
+    if(this.validateName() && this.validateType() && this.validateDescription() && !this.isNotNumberPrice() && !this.isNotNumberQuantity() && !this.isNotNumberWeight()){
+      alert("Wszystko poprawnie");
+    }else{
+      alert("Nie jest poprawnie");
+    }
   }
+
+  validateName(): boolean {
+    if(this.product1.name.length > 3){
+      return true;
+    } else {
+      return false;
+    } 
+  }
+  validateType(): boolean {
+    if(this.product1.type.length > 3){
+      return true;
+    } else {
+      return false;
+    } 
+  }
+  validateDescription(): boolean {
+    if(this.product1.description.length > 3){
+      return true;
+    } else {
+      return false;
+    } 
+  }
+  isNotNumberId(): boolean {
+  if (isNaN(this.product1.id)){
+    return true;
+  } else {
+    return false;
+  } 
+}
+isNotNumberPrice(): boolean {
+  if (isNaN(this.product1.price)){
+    return true;
+  } else {
+    return false;
+  } 
+}
+isNotNumberWeight(): boolean {
+  if (isNaN(this.product1.weight)){
+    return true;
+  } else {
+    return false;
+  } 
+}
+isNotNumberQuantity(): boolean {
+  if (isNaN(this.product1.quantity)){
+    return true;
+  } else {
+    return false;
+  } 
+}
 }
